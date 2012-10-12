ba6ac7290ab10f4f51e8584186c049957501a61b
Sei Kataoka <sei@sei-no-mba.local>
2012-10-12 13:01:30 +0900

    mongoose追加

---
+++ models/index.js
@@ -0,0 +1,25 @@
+
+/**
+ * Module dependencies.
+ */
+
+var mongoose = require('mongoose')
+  , config = require('../config').mongodb;
+
+/**
+ * Expose mongoose.
+ */
+
+exports.mongoose = mongoose;
+
+/**
+ * Expose mongoose connection.
+ */
+
+var db = exports.db = mongoose.createConnection(config.host, config.database);
+
+/**
+ * Expose models.
+ */
+
+exports.Pass = db.model('Pass', require('./pass'));
---
+++ models/pass.js
@@ -0,0 +1,14 @@
+
+/**
+ * Module dependencies.
+ */
+
+var Schema = require('mongoose').Schema
+
+/**
+ * Pass schema definition.
+ */
+
+var Schema = module.exports = new Schema({
+  
+});
