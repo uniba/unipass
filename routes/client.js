c282fc1358ce8e7c22576e395b36f350128671b5
nulltask <nulltask@gmail.com>
2012-10-24 21:36:08 +0900

    coding style

--- routes/client.js
+++ routes/client.js
@@ -9,46 +9,51 @@ var base64id = require('base64id')
   , User = schema.User;;
 
 exports.show = function(req, res) {
-  console.log('show----------------------------')
+  console.log('show----------------------------');
   reqLog(req);
   var serialNumber = req.params.serialNumber;
-  sendPkpass(serialNumber,res);
+  sendPkpass(serialNumber, res);
 };
 
 
 //post devise情報をpost
 exports.devise = function(req, res) {
-  console.log('devise----------------------------')
-  reqLog(req);
-  var deviceId = req.params.deviceId,
-  serialNumber = req.params.serialNumber,
-  authenticationToken = req.headers.authorization.replace(/ApplePass/,'').trim(),
-  pushToken = req.body.pushToken;
+  console.log('devise----------------------------');
   
-  Pass.findOne({ serialNumber : serialNumber  }, function(err, pass) {
+  reqLog(req);
+  
+  var deviceId = req.params.deviceId,
+    serialNumber = req.params.serialNumber,
+    authenticationToken = req.headers.authorization.replace(/ApplePass/,'').trim(),
+    pushToken = req.body.pushToken;
+  
+  Pass.findOne({ serialNumber: serialNumber }, function(err, pass) {
     console.log(pass);
+    
     var user = new User({
       _passbook:pass._id,
       deviceId:deviceId,
       pushToken:pushToken,
       authenticationToken:authenticationToken
-    })
-    console.log(user)
-    user.save(function(err,user){
+    });
+    
+    console.log(user);
+    
+    user.save(function(err, user) {
       if (err) return console.log(err);
-      if(pass._users.indexOf(user._id) == -1){
-        pass._users.push(user._id)
-        console.log('user._id:'+user._id) 
-        pass.save(function(err,user){
+      
+      if (pass._users.indexOf(user._id) == -1) {
+        pass._users.push(user._id);
+        console.log('user._id:'+user._id);
+        pass.save(function(err, user) {
           if (err) return console.log(err);
           res.send(201, 'registration succeeds');
         });
-      }else{
+      } else {
         res.send(200, 'the serial number is already registered for this device');
       }
     });
   });
-
 };
 
 // /v1/devices/5cb9371598cf8c76211a611d368b71d8/registrations/pass.uniba.sample/E-6NWW0hd-3I23pFAAAA
@@ -57,30 +62,33 @@ exports.devise = function(req, res) {
 // authorization:ApplePass f940cc8e3a106dbba04671cd580c85865730b64bb3064c08a969b0d7ab84ff7a540a6bd5f37d11bf4c65867031b4c9fe
 
 exports.del = function(req, res) {
-  console.log('del----------------------------')
+  console.log('del----------------------------');
+  
   reqLog(req);
+  
   var deviceId = req.params.deviceId,
-  serialNumber = req.params.serialNumber,
-  authenticationToken = req.headers.authorization;
-  User.findOne({deviceId:deviceId},function(err, user){
+    serialNumber = req.params.serialNumber,
+    authenticationToken = req.headers.authorization;
+  
+  User.findOne({ deviceId: deviceId }, function(err, user) {
     console.log(user);
-    if(user){
+    if (user) {
       user.deviceId = '';
-      user.save(function(err,user){
+      user.save(function(err,user) {
         if (err) return console.log(err);
         res.send(200, 'disassociation succeeds,');
-      })
-    }else{
-      res.send(401,'the request is not authorized')
+      });
+    } else {
+      res.send(401,'the request is not authorized');
     }
-  })
+  });
   
   res.send(200, 'succsess');
 };
 
 exports.log = function(req, res) {
-  console.log('log----------------------------')
-  console.log(res.body)
+  console.log('log----------------------------');
+  console.log(res.body);
   reqLog(req);
   res.send(201, 'succsess');
 };
