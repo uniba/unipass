cc5971389ea4ab5709b7b5acfbc3821b0dca4ce7
daichi <prinum101@gmail.com>
2012-10-26 16:31:08 +0900

    button位置

--- public/javascripts/demo.js
+++ public/javascripts/demo.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('input#demoImageBtn').change(function(){
     $('form#demoForm').trigger('submit');
   });
-  $('#demoDownload').click(function(){
+  $('#demoCapture').click(function(){
     $('input#demoImageBtn').trigger('click');
   })
 });
