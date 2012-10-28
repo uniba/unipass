f6555d7805a78f0def4e91cdab4e7e387718ae3f
nulltask <nulltask@gmail.com>
2012-10-29 00:44:13 +0900

    refactor and add spinner and ui blocker

--- public/javascripts/demo.js
+++ public/javascripts/demo.js
@@ -1,8 +1,32 @@
-$(document).ready(function(){
-  $('input#demoImageBtn').change(function(){
+$(function() {
+
+  window.scrollTo(0, 1);
+  
+  delete $.blockUI.defaults.css.border;
+  delete $.blockUI.defaults.css.backgroundColor;
+
+  $('input#demoImageBtn').on('change', function(event) {
+    var div = document.createElement('div')
+      , spinner = new Spinner({ color: '#fff' }).spin(div);
+    
+    $.blockUI({ message: div });
     $('form#demoForm').trigger('submit');
   });
-  $('#demoCapture').click(function(){
+  
+  $('#demoCapture').on('click', function(event) {
     $('input#demoImageBtn').trigger('click');
-  })
+    return false;
+  });
+  
+  $('#demoDownload').on('click', function(event) {
+    
+  });
+  
+  $('.touchButton')
+    .on('touchstart', function(event) {
+      $(this).addClass('touched');
+    })
+    .on('touchend', function(event) {
+      $(this).removeClass('touched');
+    });
 });
